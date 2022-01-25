0aded9f372c3784556d659996c5014c8de91724b
NandoBendungan <mnandopratama423@gmail.com>
2022-01-25 16:38:55 +0700

    SC BOT WA 40FITUR BUTTON

---
+++ fiturjadibot/message/mess.js
@@ -0,0 +1,19 @@
+module.exports.mess = {
+	        wait: 'Loading...',
+	        ban: 'Maaf nomor kamu sudah terbanned, Silahkan hubungi owner❗',
+			success: 'Selesai...',
+			limit: `Limit kamu sudah habis silahkan kirim .limit untuk mengecek limit`,
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'Maaf, seperti terjadi kesalahan',
+				stick: 'Itu bukan stiker kak... Hadeeh',
+				Iv: 'Link mungkin tidak valid'
+			},
+			only: {
+				group: 'Maaf❗ Command ini khusus untuk di dalam Group saja.',
+				admin: 'Maaf❗ Command ini khusus untuk admin group saja.',
+				premium: 'Kamu bukan user premium, kirim perintah *#buypremium* untuk membeli premium',
+				owner: 'Maaf❗ Command ini khusus untuk Owner Bot saja.',
+				Badmin: 'Maaf❗ Command ini khusus untuk Bot ketika jadi admin❗❗',
+			}
+		}
---
+++ message/help.js
@@ -0,0 +1,823 @@
+
+{
+  const font = '```'
+  botname = "DEFFBOTZ"
+
+
+  exports.apkmenu = (prefix) => {
+   return ` ${font}
+┌───[APK MENU  」 
+├◈${prefix}happymod text
+├◈${prefix}apkdone text
+├◈${prefix}apkgoogle text
+├◈${prefix}apkmody text 
+├◈${prefix}apkshub text
+├◈${prefix}apksolo text
+├◈${prefix}hostapk text
+├◈${prefix}revdl text 
+├◈${prefix}toraccino text 
+├◈${prefix}uapkpro text
+└─────「 ${botname} 」${font}
+`
+  }
+  exports.jadibott = (prefix) => {
+   return ` ${font}
+┌───[MULTI SESSION 」 
+├◈${prefix}jadibot *premium*
+├◈${prefix}stopjadibot 
+├◈${prefix}listbot 
+└─────「 ${botname} 」${font}
+`
+  }
+  exports.downloadmenu = (prefix) => {
+   return ` ${font}
+┌───[DOWNLOAD MENU」
+├◈${prefix}joox query
+├◈${prefix}play query
+├◈${prefix}ytsearch query
+├◈${prefix}video url_video
+├◈${prefix}fb url_video
+├◈${prefix}ig url_video
+├◈${prefix}twitter url_video
+├◈${prefix}youtube url_video
+├◈${prefix}ythd url_video
+├◈${prefix}ytmp3 url_video
+├◈${prefix}ytmp4 url_video 
+├◈${prefix}tiktok url_video
+├◈${prefix}tiktoknowm url_video
+├◈${prefix}wattpadsearch query
+├◈${prefix}spotifysearch query
+├◈${prefix}spotify url_video
+├◈${prefix}mediafire url 
+├◈${prefix}zippyshare url 
+└─────「 ${botname} 」 ${font}
+ 
+`
+  }
+  exports.funmenu = (prefix) => {
+   return ` ${font}
+┌───[FUN MENU」
+├◈${prefix}darkjoke
+├◈${prefix}meme
+├◈${prefix}citacita
+├◈${prefix}truthdare
+├◈${prefix}truth
+├◈${prefix}toxic
+├◈${prefix}dare
+├◈${prefix}waktu
+└─────「 ${botname} 」 ${font}
+  `
+  }
+  exports.grupmenu = (prefix) => {
+   return ` ${font} 
+┌───[GROUP MENU」
+├◈${prefix}antilink
+├◈${prefix}welcome enable/disable
+├◈${prefix}antivirtex
+├◈${prefix}leveling
+├◈${prefix}simi
+├◈${prefix}kick
+├◈${prefix}add
+├◈${prefix}promote
+├◈${prefix}demote
+├◈${prefix}grupsetting
+├◈${prefix}opengc
+├◈${prefix}closegc
+├◈${prefix}revoke
+├◈${prefix}setppgrup
+├◈${prefix}setdesc
+├◈${prefix}setgrupname
+├◈${prefix}tagall
+└─────「 ${botname} 」 ${font}
+
+`
+  }
+  exports.makermenu = (prefix) => {
+   return ` ${font}
+┌───[MAKER MENU」
+├◈${prefix}blackpink text
+├◈${prefix}neon text
+├◈${prefix}greenneon text
+├◈${prefix}advanceglow text
+├◈${prefix}futureneon text
+├◈${prefix}sandwriting text
+├◈${prefix}sandsummer text
+├◈${prefix}sandengraved text
+├◈${prefix}metaldark text
+├◈${prefix}neonlight text
+├◈${prefix}holographic text
+├◈${prefix}text1917 text
+├◈${prefix}minion text
+├◈${prefix}deluxesilver text
+├◈${prefix}newyearcard text
+├◈${prefix}bloodfrosted text
+├◈${prefix}halloween text
+├◈${prefix}jokerlogo text
+├◈${prefix}fireworksparkle text
+├◈${prefix}natureleaves text
+├◈${prefix}bokeh text
+├◈${prefix}toxic text
+├◈${prefix}box3d text
+├◈${prefix}roadwarning text
+├◈${prefix}breakwall text
+├◈${prefix}icecold text
+├◈${prefix}luxury text
+├◈${prefix}cloud text
+├◈${prefix}summersand text
+├◈${prefix}horrorblood text
+├◈${prefix}thunder text
+├◈${prefix}pornhub text1 text2
+├◈${prefix}glitch text1 text2
+├◈${prefix}avenger text1 text2
+├◈${prefix}space text1 text2
+├◈${prefix}ninjalogo text1 text2
+├◈${prefix}marvelstudio text1 text2
+├◈${prefix}lionlogo text1 text2
+├◈${prefix}wolflogo text1 text2
+├◈${prefix}steel3d text1 text2
+├◈${prefix}wallgravity text1 text2
+└─────「 ${botname} 」 ${font}
+
+`
+  }
+  exports.ownermenu = (prefix) => {
+   return ` ${font}
+┌───[OWNER MENU」
+├◈${prefix}broadcast
+├◈${prefix}reset
+├◈${prefix}exif
+├◈${prefix}join
+├◈${prefix}clearall
+└─────「 ${botname} 」 ${font}
+  `
+  }
+  exports.kerangmenu = (prefix) => {
+   return ` ${font}
+┌───[KERANG MENU」
+├◈${prefix}watak
+├◈${prefix}apakah
+├◈${prefix}bisakah
+├◈${prefix}kapankah
+├◈${prefix}rate
+└─────「 ${botname} 」 ${font}
+    `
+  }
+  exports.primbonmenu = (prefix) => {
+   return ` ${font}
+┌───[PRIMBON MENU」
+├◈${prefix}zodiak
+├◈${prefix}tebakumur
+├◈${prefix}tebakgender
+├◈${prefix}jadian
+├◈${prefix}weton
+├◈${prefix}jodoh
+└─────「 ${botname} 」 ${font}
+    `
+  }
+  exports.islammenu = (prefix) => {
+   return ` ${font} ${font}
+┌───[ISLAM MENU」
+├◈${prefix}alquranaudio
+├◈${prefix}asmaulhusna
+├◈${prefix}kisahnabi
+├◈${prefix}jadwalsholat
+├◈${prefix}al-maidah
+├◈${prefix}al-anam
+├◈${prefix}al-araf
+├◈${prefix}al-anfal
+├◈${prefix}at-taubah
+├◈${prefix}yunus
+├◈${prefix}hud
+├◈${prefix}yusuf
+├◈${prefix}ar-raad
+├◈${prefix}ibrahim
+├◈${prefix}al-hijr
+├◈${prefix}an-nahl
+├◈${prefix}al-isra
+├◈${prefix}al-kahfi
+├◈${prefix}maryam
+├◈${prefix}al-qariah
+├◈${prefix}at-takasur
+├◈${prefix}al-ashr
+├◈${prefix}al-humazah
+├◈${prefix}al-fil
+├◈${prefix}quraisy
+├◈${prefix}al-maun
+├◈${prefix}al-kausar
+├◈${prefix}al-kafirun
+├◈${prefix}an-nashr
+├◈${prefix}al-lahab
+├◈${prefix}al-ikhlas
+├◈${prefix}al-falaq
+├◈${prefix}an-nas
+└─────「 ${botname} 」${font}
+    `
+  }
+  exports.rpgmenu = (prefix) => {
+   return ` ${font}
+┌───[RPG MENU」
+├◈ ${prefix}adventure *memulai*
+├◈ ${prefix}usepotion *memakai potion*
+├◈ ${prefix}heal  *cek heal*
+├◈ ${prefix}potion *cek potion* 
+├◈ ${prefix}buypotion 
+└─────「 ${botname} 」 ${font}
+    `
+  }
+  exports.askaramenu = (prefix) => {
+   return ` ${font}
+┌───[ASKARA MENU」
+├◈ ${prefix}latinkejawa
+├◈ ${prefix}jawakelatin
+├◈ ${prefix}latinkesunda
+├◈ ${prefix}sundakelatin
+└─────「 ${botname} 」 ${font}
+    `
+  }
+  exports.photodunia = (prefix) => {
+   return ` ${font}
+┌───[PHOTO DUNIA」
+├◈ ${prefix}christmas
+├◈ ${prefix}cemeterygates
+├◈ ${prefix}fortunecookie
+├◈ ${prefix}pendant
+├◈ ${prefix}airballoon
+├◈ ${prefix}rugbyball
+├◈ ${prefix}lightwriting
+├◈ ${prefix}blinkinglights
+├◈ ${prefix}denimembroidery
+├◈ ${prefix}bracelet
+├◈ ${prefix}nightmare
+├◈ ${prefix}lightgraffiti
+├◈ ${prefix}
+└─────「 ${botname} ${font}
+    `
+  }
+  exports.randomtag = (prefix) => {
+   return ` ${font}
+┌───[RANDOM TAG」
+├◈ ${prefix}ganteng
+├◈ ${prefix}cantik
+├◈ ${prefix}jelek
+├◈ ${prefix}goblok
+├◈ ${prefix}monyet
+├◈ ${prefix}bego
+├◈ ${prefix}pinter
+├◈ ${prefix}jago
+├◈ ${prefix}nolep
+├◈ ${prefix}babi
+├◈ ${prefix}baik
+├◈ ${prefix}beban
+├◈ ${prefix}jahat
+├◈ ${prefix}anjing
+├◈ ${prefix}haram
+├◈ ${prefix}wibu
+├◈ ${prefix}kontol
+├◈ ${prefix}pakboy
+├◈ ${prefix}pakgirl
+├◈ ${prefix}hebat
+├◈ ${prefix}sadboy
+├◈ ${prefix}sadgirl
+└─────「 ${botname} 」 ${font}
+    `
+  }
+  exports.gamemenu = (prefix) => {
+   return ` ${font}
+┌───[GAME MENU」
+├◈ ${prefix}truth
+├◈ ${prefix}dare  
+├◈ ${prefix}slot
+├◈ ${prefix}suit
+├◈ ${prefix}gelud
+├◈ ${prefix}tebakkata
+├◈ ${prefix}tebakkimia
+├◈ ${prefix}tebaklirik
+├◈ ${prefix}math
+├◈ ${prefix}siapaaku
+├◈ ${prefix}tebakanime
+├◈ ${prefix}tebakbendera
+├◈ ${prefix}tictactoe
+└─────「 ${botname} 」 ${font}
+    `
+  }
+  exports.animemenu = (prefix) => {
+   return ` ${font}
+┌───[ANIME MENU」
+├◈${prefix}itsuki
+├◈${prefix}nino
+├◈${prefix}yotsuba
+├◈${prefix}naruto
+├◈${prefix}mikasa
+├◈${prefix}eren
+├◈${prefix}titan
+├◈${prefix}levi
+├◈${prefix}sakura
+├◈${prefix}hinata
+├◈${prefix}minato
+├◈${prefix}jiraya
+├◈${prefix}tsunade
+├◈${prefix}kiba
+├◈${prefix}armin
+├◈${prefix}bertholdt
+├◈${prefix}erwin
+├◈${prefix}reiner
+├◈${prefix}annie
+├◈${prefix}hanji
+├◈${prefix}jean
+├◈${prefix}connie
+├◈${prefix}historia
+├◈${prefix}sasha
+├◈${prefix}boruto
+├◈${prefix}sarada
+├◈${prefix}sasuke
+├◈${prefix}madara
+├◈${prefix}obito
+├◈${prefix}kanao
+├◈${prefix}sayu
+├◈${prefix}yoshida
+├◈${prefix}airi
+├◈${prefix}asami
+├◈${prefix}yuzuha
+├◈${prefix}hashimoto
+├◈${prefix}tanLarasCans
+├◈${prefix}nezuko
+├◈${prefix}ichika
+├◈${prefix}luffy
+├◈${prefix}zenitsu
+├◈${prefix}giyu
+├◈${prefix}sakonji
+├◈${prefix}inosuke
+├◈${prefix}zoro
+├◈${prefix}sanji
+├◈${prefix}sagiri
+├◈${prefix}killua
+├◈${prefix}taiko
+├◈${prefix}natsu
+├◈${prefix}genos
+├◈${prefix}saitama
+├◈${prefix}miku
+└─────「 ${botname} 」 ${font}
+    `
+  }
+  exports.ephoto360 = (prefix) => {
+   return ` ${font}
+┌───[EPHOTO 360」
+├◈ ${prefix}shadow text
+├◈ ${prefix}cup text
+├◈ ${prefix}cup1 text
+├◈ ${prefix}romance text 
+├◈ ${prefix}smoke text
+├◈ ${prefix}burnpaper text
+├◈ ${prefix}lovemessage text
+├◈ ${prefix}undergrass text 
+├◈ ${prefix}love text 
+├◈ ${prefix}coffe text
+├◈ ${prefix}woodheart text
+├◈ ${prefix}woodenboard text
+├◈ ${prefix}summer3d text
+├◈ ${prefix}wolfmetal text
+├◈ ${prefix}nature3d text
+├◈ ${prefix}underwater text
+├◈ ${prefix}golderrose text
+├◈ ${prefix}summernature text
+├◈ ${prefix}letterleaves text
+├◈ ${prefix}glowingneon text
+├◈ ${prefix}fallleaves text
+├◈ ${prefix}flamming text
+├◈ ${prefix}harrypotter text
+├◈ ${prefix}carvedwood text
+├◈ ${prefix}arcade8bit text
+├◈ ${prefix}battlefield4 text1 text2 
+├◈ ${prefix}pubg text1 text2
+    `
+  }
+  exports.hewanmenu = (prefix) => {
+   return ` ${font}
+┌───[HEWAN MENU」
+├◈${prefix}fox
+├◈${prefix}panda
+├◈${prefix}panda2
+├◈${prefix}bird
+├◈${prefix}dog
+├◈${prefix}cat
+├◈${prefix}koala
+└─────「 ${botname} 」 ${font}
+`
+  }
+  exports.animanga = (prefix) => {
+   return ` ${font}
+┌───[ANIMANGA」
+├◈${prefix}anime
+├◈${prefix}manga
+├◈${prefix}character
+├◈${prefix}kusonime
+├◈${prefix}kusonimesearch
+├◈${prefix}otakudesu
+├◈${prefix}otakudesusearch
+└─────「 ${botname} 」 ${font}
+`
+  }
+  exports.nulismenu = (prefix) => {
+   return ` ${font}
+┌───[NULIS MENU」
+├◈${prefix}nulis1 *text*
+├◈${prefix}nulis2 *text*
+├◈${prefix}nulis4 *text*
+├◈${prefix}nulis4 *text*
+└─────「 ${botname} 」 ${font}
+`
+  }
+  exports.textgenerator = (prefix) => {
+   return ` ${font}
+┌───[TEXT GENERATOR」
+├◈${prefix}holoh
+├◈${prefix}hklkh
+├◈${prefix}halah
+├◈${prefix}hilih
+├◈${prefix}huluh
+├◈${prefix}heleh
+├◈├◈${prefix}alay
+├◈${prefix}bahasapurba
+├◈${prefix}randombahasa
+├◈${prefix}namaninja
+├◈${prefix}texttomorse *text*
+├◈${prefix}morsetotext *kode morse*
+└─────「 ${botname} 」 ${font}
+`
+  }
+  exports.stickermenu = (prefix) => {
+   return ` ${font}
+┌───[STICKER MENU」
+├◈${prefix}ttp *txt*
+├◈${prefix}ttp2 *txt*
+├◈${prefix}ttp3 *txt*
+├◈${prefix}ttp4 *txt*
+├◈${prefix}attp *txt*
+├◈${prefix}attp2 *txt*
+├◈${prefix}ttpblue
+├◈${prefix}ttpgreen
+├◈${prefix}ttpred
+├◈${prefix}ttpyellow
+└─────「 ${botname} 」 ${font}
+`
+  }
+  exports.ephotooxy = (prefix) => {
+   return ` ${font}
+┌───[EPHOTO OXY」
+├◈${prefix}wetglass text
+├◈${prefix}multicolor3d text
+├◈${prefix}watercolor text
+├◈${prefix}luxurygold text
+├◈${prefix}galaxywallpaper text
+├◈${prefix}lighttext text
+├◈${prefix}beautifulflower text
+├◈${prefix}puppycute text 
+├◈${prefix}royaltext text 
+├◈${prefix}heartshaped text
+├◈${prefix}birthdaycake text
+├◈${prefix}galaxystyle text
+├◈${prefix}hologram3d text
+├◈${prefix}greenneon text
+├◈${prefix}glossychrome text
+├◈${prefix}greenbush text
+├◈${prefix}metallogo text
+├◈${prefix}noeltext text
+├◈${prefix}glittergold text
+├◈${prefix}textcake text
+├◈${prefix}starsnight text
+├◈${prefix}wooden3d text
+├◈${prefix}textbyname text
+├◈${prefix}writegalacy text
+├◈${prefix}galaxybat text
+├◈${prefix}snow3d text
+├◈${prefix}birthdayday text
+├◈${prefix}goldplaybutton text
+├◈${prefix}silverplaybutton text
+├◈${prefix}freefire text 
+└─────「 ${botname} 」 ${font}
+
+    `
+  }
+  exports.sertimenu = (prefix) => {
+   return ` ${font}
+┌───[SERTI & LOGO MENU」
+├◈${prefix}gfx1
+├◈${prefix}gfx2
+├◈${prefix}gfx3  
+├◈${prefix}badboyserti
+├◈${prefix}badgirlserti
+├◈${prefix}sadboyserti
+├◈${prefix}sadgirlserti
+├◈${prefix}goodgirlsrti
+├◈${prefix}goodboyserti
+├◈${prefix}bucinserti
+├◈${prefix}fuckboyserti
+└─────「 ${botname} 」 ${font}
+      `
+  }
+  exports.filmmenu = (prefix) => {
+   return ` ${font}
+┌───[FILM & STORY」
+├◈${prefix}wattpad
+├◈${prefix}wattpadsearch
+├◈${prefix}drakorongoing
+├◈${prefix}genshinsearch
+└─────「 ${botname} 」 ${font}
+        `
+  }
+  exports.editormenu = (prefix) => {
+   return ` ${font}
+┌───[EDITOR MENU」
+├◈${prefix}affect
+├◈${prefix}blur
+├◈${prefix}rip
+├◈${prefix}trash
+├◈${prefix}wasted
+├◈${prefix}wasted2
+├◈${prefix}glass
+├◈${prefix}invert
+├◈${prefix}sepia
+├◈${prefix}circle
+├◈${prefix}wanted
+├◈${prefix}joker
+└─────「 ${botname} 」 ${font}
+
+CARA MENGGUNAKAN FITUR INI YAITU
+KIRIM GAMBAR TRUS REPLY DENGAN COMMAND DIATAS
+          `
+  }
+  exports.infomenu = (prefix) => {
+   return ` ${font}
+┌───[INFO MENU」
+├◈${prefix}infogempa
+├◈${prefix}coronaglobal
+├◈${prefix}wikipedia
+├◈${prefix}newsinfo
+├◈${prefix}cuaca
+├◈${prefix}infobeasiswa
+├◈${prefix}hoax
+├◈${prefix}kbbi
+├◈${prefix}jadwaltvnow
+├◈${prefix}jadwalbola
+└─────「 ${botname} 」 ${font}
+    
+
+
+          `
+  }
+  exports.othermenu = (prefix) => {
+   return ` ${font}
+┌───「OTHER MENU」
+├◈${prefix}translate
+├◈${prefix}lirik
+├◈${prefix}jarak
+├◈${prefix}heroml
+├◈${prefix}chord
+└─────「 ${botname} 」 ${font}         
+           `
+  }
+  exports.anonymous = (prefix) => {
+   return ` ${font}
+┌───「ANONYMOUS」
+├◈${prefix}start *memulai pencarian*
+├◈${prefix}next *mencari yang lain*
+└─────「 ${botname} 」 ${font}         
+           `
+  }
+  exports.tembakmenu = (prefix) => {
+   return ` ${font}
+┌───[TEMBAK MENU」
+├◈${prefix}tembak darat
+├◈${prefix}tembak laut
+├◈${prefix}tembak udara
+├◈${prefix}maling rumah
+├◈${prefix}maling gedung
+└─────「 ${botname} 」 ${font}
+
+           `
+  }
+  exports.rules = (prefix) => {
+   return ` ${font}
+ ╔════════ ≪ °❈° ≫ ════════╗  
+   RULES DEFFBOTZ 
+   1. Dilarang spam
+   2. Jangan Telpon Bot
+   3. Chat Owner Jika penting
+   4. Bot Menjaga Privasi Kamu
+   5. Jangan Culik Bot
+   6. Diharapkan Kamu Memahami Bot
+ ╚════════ ≪ °❈° ≫ ════════╝        
+                     `
+  }
+  exports.textrandom = (prefix) => {
+   return ` ${font}
+┌───[RANDOM TEXT」
+├◈${prefix}quotes
+├◈${prefix}quotesanime
+├◈${prefix}quotesdilan
+├◈${prefix}quotesimage
+├◈${prefix}quotesislami
+├◈${prefix}creepyfact
+├◈${prefix}animequotes
+├◈${prefix}katagalau
+├◈${prefix}katagalau2
+├◈${prefix}fml
+├◈${prefix}trumpquotest
+├◈${prefix}trumpthink
+├◈${prefix}katasenja
+├◈${prefix}motivasi
+├◈${prefix}faktaunik
+├◈${prefix}katabijak
+├◈${prefix}bucin
+├◈${prefix}pantun
+├◈${prefix}randomnama
+└─────「 ${botname} 」 ${font}
+  
+             `
+  }
+  exports.randomimage = (prefix) => {
+   return ` ${font}
+┌───[RANDOM IMAGE」
+├◈${prefix}art
+├◈${prefix}bts
+├◈${prefix}exo
+├◈${prefix}elf
+├◈${prefix}neko
+├◈${prefix}waifu
+├◈${prefix}shota
+├◈${prefix}husbu
+├◈${prefix}shinobu
+├◈${prefix}megumin
+├◈${prefix}wallnime
+└─────「 ${botname} 」 ${font}
+  
+             `
+  }
+  exports.kartunmenu = (prefix) => {
+   return ` ${font}
+┌───[KARTUN MENU」
+├◈${prefix}kartundrama
+├◈${prefix}kartunadventure    
+├◈${prefix}kartunaction
+├◈${prefix}kartunmovie
+└─────「 ${botname} 」 ${font}
+      `
+  }
+  exports.asupanmenu = (prefix) => {
+   return ` ${font}
+┌───[ASUPAN MENU」
+├◈${prefix}bocil
+├◈${prefix}chika
+├◈${prefix}delvira
+├◈${prefix}ayu
+├◈${prefix}viona
+├◈${prefix}yana
+├◈${prefix}bunga
+├◈${prefix}aura
+├◈${prefix}ziva
+├◈${prefix}syania
+├◈${prefix}riri
+├◈${prefix}syifa
+├◈${prefix}mama_gina
+├◈${prefix}alackenya
+├◈${prefix}mangayutri
+├◈${prefix}geayubi
+├◈${prefix}ukhty
+├◈${prefix}santuy
+└─────「 ${botname} 」 ${font}
+      `
+  }
+  exports.mememaker = (prefix) => {
+   return ` ${font}
+  ┌───[MEME MAKER」
+├◈${prefix}meme1
+├◈${prefix}meme2
+├◈${prefix}meme3
+├◈${prefix}cmm
+├◈${prefix}ohno
+  └─────「 ${botname} 」 ${font}
+  
+              `
+  }
+  exports.stalkmenu = (prefix) => {
+   return ` ${font}
+┌───[STALK MENU」
+├◈${prefix}twiterstalk
+├◈${prefix}igstalk 
+├◈${prefix}mlstalk
+└─────「 ${botname} 」 ${font}
+
+              `
+  }
+  exports.soundmenu = (prefix) => {
+   return ` ${font}
+┌───[SOUND MENU」
+├◈${prefix}audio1
+├◈${prefix}audio2
+├◈${prefix}audio3
+├◈${prefix}audio4
+├◈${prefix}audio5
+├◈${prefix}audio6
+├◈${prefix}audio7
+├◈${prefix}audio8
+├◈${prefix}audio9
+├◈${prefix}audio10
+└─────「 ${botname} 」 ${font}
+
+      `
+  }
+  exports.convertmenu = (prefix) => {
+   return ` ${font}
+┌───[CONVERT MENU」
+├◈${prefix}sticker
+├◈${prefix}tomp3
+├◈${prefix}tomp4
+├◈${prefix}togif
+├◈${prefix}toimg
+├◈${prefix}tourl
+├◈${prefix}slow
+├◈${prefix}gemuk
+├◈${prefix}gemuk2
+├◈${prefix}bass
+├◈${prefix}ghost
+├◈${prefix}hode
+├◈${prefix}nightcore
+├◈${prefix}fast
+├◈${prefix}tupai
+├◈${prefix}tupai2
+├◈${prefix}blub
+└─────「 ${botname} 」 ${font}
+
+      `
+  }
+  exports.cecanmenu = (prefix) => {
+   return ` ${font}
+┌───[CECAN MENU」
+├◈${prefix}cecanindo
+├◈${prefix}cecanmalysia
+├◈${prefix}cecanjapan
+├◈${prefix}cecanchina
+├◈${prefix}cecankorea
+└─────「 ${botname} 」 ${font}
+
+      `
+  }
+  exports.coganmenu = (prefix) => {
+   return ` ${font}
+┌───[COGAN MENU」
+├◈${prefix}coganindo
+├◈${prefix}coganmalaysia
+├◈${prefix}coganjapan
+├◈${prefix}coganchina
+├◈${prefix}cogankorea
+└─────「 ${botname} 」 ${font}
+
+      `
+  }
+  exports.searchmenu = (prefix) => {
+   return ` ${font}
+┌───[SEARCH MENU」
+g├◈${prefix}image
+├◈${prefix}gimage2
+├◈${prefix}konachan
+├◈${prefix}wallpapersearch
+├◈${prefix}wallpapersearch2
+├◈${prefix}shopee
+└─────「 ${botname} 」 ${font}
+
+      `
+  }
+  exports.koreamenu = (prefix) => {
+   return ` ${font}
+┌───[KOREA MENU」
+├◈${prefix}blackpinkk
+├◈${prefix}twice
+├◈${prefix}lovely
+├◈${prefix}lightsum
+├◈${prefix}bugaboo
+├◈${prefix}rainbow
+├◈${prefix}majors
+├◈${prefix}secret
+├◈${prefix}pixy
+└─────「 ${botname} 」 ${font}
+
+      `
+  }
+  exports.aboutmenu = (prefix) => {
+   return ` ${font}
+      ┌───[ABOUT MENU」
+    ├◈${prefix}speed
+    ├◈${prefix}runtime 
+    ├◈${prefix}owner
+    ├◈${prefix}botstatus
+    ├◈${prefix}rules
+      └─────「 ${botname} 」 ${font}
+      
+                    `
+
+  }
+
+}
+
+
+
